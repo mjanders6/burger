66d11c3b63c795a0e0d07bf1d811cf874d090e0a
Michael Anderson <mjanders6@gmail.com>
2019-05-21 19:44:03 -0700

    heroku update

--- config/index.js
+++ config/index.js
@@ -1,10 +1,19 @@
 require('dotenv').config()
 const { createConnection } = require('mysql2')
+const connection
 
-module.exports = createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: process.env.SQL_PASSWORD,
-  database: 'burger_db'
-})
+if (process.env.JAWSDB_ULR) {
+  connection = createConnection(process.env.JAWSDB_ULR)
+} else {
+  connection = createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.SQL_PASSWORD,
+    database: 'burger_db'
+  })
+}
+
+connection.connect()
+module.exports = connection
+
